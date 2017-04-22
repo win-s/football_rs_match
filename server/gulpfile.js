cdc385895c85d14661e70ccd537ce3075e8392ec
Asawin Sattiya <desperandro@gmail.com>
2017-04-22 17:04:31 +0700

    automate public eonasdan datepicker

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -3,8 +3,8 @@ var gulp = require('gulp');
 gulp.task('public_eonasdan',()=>{
     var eonasdan_bootstrap_datetimepicker = [
         { src:'bower_components/moment/min/moment.min.js',des:'public/js/eonasdan' },
-        { src:'bower_components/eonasdan-bootstrap-datetimepicker/build/bootstrap-datetimepicker.min.js',des:'public/js/eonasdan' },
-        { src:'bower_components/eonasdan-bootstrap-datetimepicker/build/',des:'public/stylesheets/eonasdan' },
+        { src:'bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',des:'public/js/eonasdan' },
+        { src:'bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css',des:'public/css/eonasdan' },
     ];
 
     eonasdan_bootstrap_datetimepicker.forEach((item)=>{
@@ -14,5 +14,5 @@ gulp.task('public_eonasdan',()=>{
 });
 
 gulp.task('public',[
-    public_eonasdan
+    "public_eonasdan"
 ]);
